50e12535bc5cff2353d0789a902c36ae8d5e4100
absolve <wangding1992@126.com>
2025-11-23 23:04:36 +0800

    更新功能

--- machine-td/script/enemy.gd
+++ machine-td/script/enemy.gd
@@ -6,3 +6,6 @@ var vec=Vector2.ZERO
 var target=[] #目标
 var points=[]
 var pointIndex=0
+
+@onready var base=$base
+@onready var turret=$turret
--- machine-td/script/mini_tank.gd
+++ machine-td/script/mini_tank.gd
@@ -2,17 +2,17 @@ extends "res://script/enemy.gd"
 
 
 func _ready():
-	speed=1000
+	speed=100
 	
 
 func _physics_process(delta):
 	if points.size()==0:
 		return 
-	var target =points[pointIndex]
-	print(position.distance_to(target))
-	if position.distance_to(target) < 1:
-		print(pointIndex)
+	var target1 =points[pointIndex]
+	if position.distance_to(target1) < 1:
+		#print("====",pointIndex,points[pointIndex])
 		pointIndex=wrapi(pointIndex+1, 0,points.size())
-		target =points[pointIndex]
-	vec = (target - position).normalized() * speed
-	position+=vec*delta
+		target1 =points[pointIndex]
+	#vec = (target - position).normalized() * speed
+	position+=position.direction_to(target1) * speed * delta
+	base.look_at(target1)
