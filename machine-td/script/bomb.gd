699b1300dc33e90c80dd401a2da7fff3c05a4b33
absolve <wangding1992@126.com>
2026-02-09 23:55:26 +0800

    更新功能

--- machine-td/script/bomb.gd
+++ machine-td/script/bomb.gd
@@ -10,23 +10,19 @@ var hasDamage = false
 
 
 func _ready():
-    pass
+	pass
 
 func _physics_process(_delta):
-    if hasDamage:
-        for i in objList:
-            if i is Enemy:
-                i.hurt(damage)
-        hasDamage = false
+	if hasDamage:
+		for i in objList:
+			if i is Enemy:
+				i.hurt(damage)
+		hasDamage = false
 
  
 func _on_area_shape_exited(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int):
-    objList.erase(area)
+	objList.erase(area)
 
 
 func _on_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int):
-    objList.append(area)
-
-
-
-
+	objList.append(area)
