b306befd93856c8d8ff9d7faf70558813ce701c1
absolve <wangding1992@126.com>
2026-03-02 23:44:01 +0800

    更新功能

---
+++ machine-td/script/mediumTank.gd
@@ -0,0 +1,22 @@
+extends "res://script/enemy.gd"
+
+var parent:PathFollow2D
+
+func _ready() -> void:
+	parent=get_parent()
+
+func hurt(_num):
+	hp -= _num
+	lifeBar.value = hp
+	if hp < 0:
+		ExplosionManage.playExplosion(global_position)
+		Game.defeatEnemy.emit(reward)
+		owner.queue_free()
+
+func _physics_process(_delta):
+	if points.size() == 0:
+		return
+	parent.progress+=speed*_delta	
+	if parent.progress_ratio>=1:
+		Game.enemyEscape.emit(lossPoints)
+		owner.queue_free()
